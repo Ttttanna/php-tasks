4c8c560cbb974d46ea7c96a382ca9594edea9d1a
Tatsiana Alsheuskaya <ttttanna@gmail.com>
2020-10-28 00:02:29 +0300

    feat: arrays solutions

--- arrays.php
+++ arrays.php
@@ -62,4 +62,49 @@ $products = [
 
 	}
 
-//
+//Arrays
+//1.Сумма положительных четных элементов массива
+$array = [1,-2,-3, 10, 2,-1, 3, 13, 9];
+$sum = 0;
+foreach( $array as $num ){
+    if ($num > 0 && $num % 2 === 0) {
+        $sum+=$num;
+    }
+}
+
+print_r($sum);
+
+//2.Максимальное значение среди четных индексов
+$maxElement = 0;
+$count = count($array);
+for ($i = 0; $i <= $count; $i++, $i++) {
+    if ($array[$i] < $array[$i + 2]) {
+        $maxElement = $array[$i+2];
+    } else {
+        $maxElement = $array[$i];
+    }
+}
+
+print_r($maxElement); // выводится ошибка изза того что я обращаюсь к элементу массива которого нет
+
+// 3.Элементы массива которые меньше среднеарифметического
+$middleSum = array_sum($array)/$count;
+foreach( $array as $index) {
+    if ($index < $middleSum) {
+        var_dump($index);
+    }
+}
+
+// 4.два минимальных элемента массива
+sort($array, SORT_NUMERIC);
+var_dump($array[0]);
+var_dump($array[1]);
+
+//5.Cжать массив удалив элементы чье значение в интервале [0, 3], дополнить до длины нулями
+for ($i = 0; $i < $count; $i++) {
+	if($array[$i] >= 0 && $array[$i] <= 3) {
+		array_splice($array, $i, 1);
+		$i--;
+	}
+}
+array_pad($array, $count, 0);
